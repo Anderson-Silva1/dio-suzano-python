e7fdfbcba96c27e31a9b3c963dc1668f0813e967
Anderson Dev <168702409+Anderson-Silva1@users.noreply.github.com>
2025-05-20 07:46:12 -0300

    Atualizar o 005-metodos.py

--- 003-colecoes/001-listas/pratica/005-metodos.py
+++ 003-colecoes/001-listas/pratica/005-metodos.py
@@ -11,3 +11,13 @@ print(append_list) # [1, "Python", [1, 2, 3]]
 clear_list = append_list
 clear_list.clear()
 print(clear_list) # []
+
+# count
+count_list = ["azul", "amarelo", "vermelho", "azul"]
+print(count_list.count("azul"))
+
+# extend
+extend_list = [1,2,3,4]
+extend_list2 = [5,6,7,8]
+extend_list.extend(extend_list2)
+print(extend_list)
